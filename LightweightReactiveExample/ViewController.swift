2e38a8985f10fc0ac4f93a11dae4747b636e8904
hidemune.takahashi <hidemune.takahashi@outlook.com>
2017-09-22 01:28:27 +0900

    Implement test codes.

--- LightweightReactiveExample/ViewController.swift
+++ LightweightReactiveExample/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import LightweightReactive
 
 class ViewController: UIViewController {
 
+    @objc dynamic var reactTest: String = "Initial Value"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        LightweightReactive.observer.startObserving(source: self, keyPath: #keyPath(ViewController.reactTest), options: [.new, .old], context: nil) { (keyPath, object, changes, context) in
+            print("reacted!")
+        }
+        reactTest = "Lightweight Reactive"
     }
 
     override func didReceiveMemoryWarning() {
